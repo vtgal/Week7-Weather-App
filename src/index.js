12ff57984fdac3f41b92ee1f7869c7e9f3597889
vtgal <vtskyle101@gmail.com>
2024-08-27 09:55:04 -0500

    added my api code.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+function searchCity(city) {
+let apiKey = "6e4744f50a4o233acde7cb8355a181t3";
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
