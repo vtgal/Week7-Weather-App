72da003bad29d1bd0a21734652e9a406b2ffd472
vtgal <vtskyle101@gmail.com>
2024-08-27 09:33:24 -0500

    Added content into my index. js.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 function handleSearchSubmit(event) {
     event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
