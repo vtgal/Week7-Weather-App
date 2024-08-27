62c724d55401438ff016b9bd6094f8d420a54530
vtgal <vtskyle101@gmail.com>
2024-08-27 11:51:13 -0500

    adjusted line space.

--- src/index.js
+++ src/index.js
@@ -6,29 +6,28 @@ let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windSpeedElement = document.querySelector("#wind-speed");
 let timeElement = document.querySelector("#time");
+let date = new Date(response.data.time * 1000);
+
+
 
 cityElement.innerHTML = response.data.city;
-timeElement.innerHTML = "Tuesday 14:49";
+
+timeElement.innerHTML = date.getHours();
 descriptionElement.innerHTML = response.data.condition.description;
 humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 temperatureElement.innerHTML = Math.round(temperature);
 }
 
-
-
 function searchCity(city) {
 let apiKey = "6e4744f50a4o233acde7cb8355a181t3";
-let apiUrl =
-  `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
   axios.get(apiUrl).then(refreshWeather);
 }
-
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-
-    searchCity(searchInput.value);
+searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
