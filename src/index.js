e21c0c4248d6b93155da81696be923479f6e6789
vtgal <vtskyle101@gmail.com>
2024-08-27 18:52:12 -0500

    Fixing error with humidity.

--- src/index.js
+++ src/index.js
@@ -3,19 +3,17 @@ let temperatureElement = document.querySelector("#temperature");
 let temperature = response.data.temperature.current;
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
-let humidityElement = document.querySelector("#humidity");
-let windSpeedElement = document.querySelector("#wind-speed");
 let timeElement = document.querySelector("#time");
 let date = new Date(response.data.time * 1000);
 let iconElement = document.querySelector("#icon");
 
-iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`; 
+console.log(response.data);
+
 cityElement.innerHTML = response.data.city;
 timeElement.innerHTML = formatDate(date);
 descriptionElement.innerHTML = response.data.condition.description;
-humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 temperatureElement.innerHTML = Math.round(temperature);
+iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 function formatDate(date) {
