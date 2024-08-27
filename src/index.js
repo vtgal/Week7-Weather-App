19a058a4f1052e7361e67d60245d6133d4cda9d8
vtgal <vtskyle101@gmail.com>
2024-08-27 11:04:30 -0500

    Added an element html for my humidity.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ let temperatureElement = document.querySelector("#temperature");
 let temperature = response.data.temperature.current;
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
 
 cityElement.innerHTML = response.data.city;
 descriptionElement.innerHTML = response.data.condition.description;
+humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 temperatureElement.innerHTML = Math.round(temperature);
 }
 
