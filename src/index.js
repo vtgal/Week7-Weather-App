76cf6c05f8297f2aa6cb68c1bcdbf74860e9f029
vtgal <vtskyle101@gmail.com>
2024-08-27 11:10:41 -0500

    added element for my windspeed.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ let temperature = response.data.temperature.current;
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
+let windSpeedElement = document.querySelector("#wind-speed");
 
 cityElement.innerHTML = response.data.city;
 descriptionElement.innerHTML = response.data.condition.description;
 humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 temperatureElement.innerHTML = Math.round(temperature);
 }
 
