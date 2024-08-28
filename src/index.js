c1cd08c75c8108a1670d238c7f865029543d7280
vtgal <vtskyle101@gmail.com>
2024-08-28 09:18:20 -0500

    Added my humidity element.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ function refreshWeather(response) {
  let temperature = response.data.temperature.current;
  let cityElement = document.querySelector("#city");
  let descriptionElement = document.querySelector("#description");
-
- let windSpeedElement = document.querySelector("#wind-speed");
+let humidityElement = document.querySelector("#humidity");
+let windSpeedElement = document.querySelector("#wind-speed");
  let timeElement = document.querySelector("#time");
  let date = new Date(response.data.time * 1000);
  let iconElement = document.querySelector("#icon");
@@ -12,7 +12,7 @@ function refreshWeather(response) {
  cityElement.innerHTML = response.data.city;
  timeElement.innerHTML = formatDate(date);
  descriptionElement.innerHTML = response.data.condition.description;
- 
+ humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
  temperatureElement.innerHTML = Math.round(temperature);
  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
